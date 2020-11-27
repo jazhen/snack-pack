6626124ac469ef07104c269e6a9a99b15063d01c
Jason Zhen <jasonzhen.mail@gmail.com>
2020-11-26 22:26:51 -0800

    add slash to webpack prod public path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   devtool: false,
   output: {
     path: paths.build,
-    publicPath: '/snack-pack',
+    publicPath: '/snack-pack/',
     filename: 'js/[name].[contenthash].bundle.js',
   },
   plugins: [
