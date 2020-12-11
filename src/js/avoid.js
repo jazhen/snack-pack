b9f7fb4c2e414cd9fda9b053230d13ac52c7af99
Jason Zhen <jasonzhen.mail@gmail.com>
2020-12-10 23:21:31 -0800

    avoid game ball bounces off walls

--- src/js/avoid.js
+++ src/js/avoid.js
@@ -7,24 +7,47 @@ class Avoid {
     this.transitionText = 'avoid';
     this.fps = 24;
 
-    this.x = this.canvas.canvas.width / 2;
-    this.y = this.canvas.canvas.height / 2;
-    this.dx = 2;
-    this.dy = -2;
+    this.enemy = {
+      radius: 10,
+      x: this.canvas.canvas.width / 2,
+      y: this.canvas.canvas.height / 2,
+      dx: 2,
+      dy: -2,
+    };
   }
 
   draw() {
     // debugger;
     this.canvas.ctx.beginPath();
-    this.canvas.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
-    this.canvas.ctx.fillStyle = '#ffffff';
+    this.canvas.ctx.arc(
+      this.enemy.x,
+      this.enemy.y,
+      this.enemy.radius,
+      0,
+      Math.PI * 2
+    );
+    this.canvas.ctx.fillStyle = '#0095DD';
     this.canvas.ctx.fill();
     this.canvas.ctx.closePath();
   }
 
   update() {
-    this.x += this.dx;
-    this.y += this.dy;
+    // debugger;
+    if (
+      this.enemy.x + this.enemy.dx > 400 - this.enemy.radius ||
+      this.enemy.x + this.enemy.dx < this.enemy.radius
+    ) {
+      this.enemy.dx = -this.enemy.dx;
+    }
+    if (
+      this.enemy.y + this.enemy.dy > 300 - this.enemy.radius ||
+      this.enemy.y + this.enemy.dy < this.enemy.radius
+    ) {
+      this.enemy.dy = -this.enemy.dy;
+    }
+
+    this.enemy.x += this.enemy.dx;
+    this.enemy.y += this.enemy.dy;
   }
 
   play() {
